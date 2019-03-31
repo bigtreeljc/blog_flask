c2fb7050ad05503817bda7075e3c9c46a83f918a
Tree Lu <treelu@mac-jlu.local>
2019-03-31 15:19:40 +0800

    account update finished

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,9 +1,11 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, \
     SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, \
     EqualTo, ValidationError
 from .models import User, Post
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
   username = StringField('UserName',
@@ -36,3 +38,22 @@ class LoginForm(FlaskForm):
   remember = BooleanField("remeber me")
   submit = SubmitField('Sign In')
 
+class UpdateAccountForm(FlaskForm):
+  username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+  email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+  picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+  submit = SubmitField('Update')
+
+  def validate_username(self, username):
+    if username.data != current_user.username:
+      user = User.query.filter_by(username=username.data).first()
+      if user:
+        raise ValidationError('That username is taken. Please choose a different one.')
+
+  def validate_email(self, email):
+    if email.data != current_user.email:
+      user = User.query.filter_by(email=email.data).first()
+      if user:
+        raise ValidationError('That email is taken. Please choose a different one.')
